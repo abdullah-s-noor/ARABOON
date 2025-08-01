d68e4ae9d3660466f3d6e05370bc66aaa71b34d8
AbdullahNoor <abdullah.s.noor04@gmail.com>
2025-08-01 16:03:13 +0300

    render1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:process.env.VITE_BASE_PATH||'/araboon',
+    plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
+  server: {
+    historyApiFallback: true
+  }
 })
