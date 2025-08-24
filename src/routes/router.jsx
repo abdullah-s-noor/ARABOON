73a67791e8e4fac37f682df495efc27ca6f258c7
AbdullahNoor <abdullah.s.noor04@gmail.com>
2025-08-24 08:07:54 +0300

    build a cover image edition

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import AdminLayout from "../components/layouts/admin/AdminLayout.jsx";
 import UserLayout from "../components/layouts/user/UserLayout.jsx";
 import AdminHomePage from "../pages/admin/Home.jsx";
-import { UserHomePage, MangaInformation, MangaList, Library, MangaRanking,TestPage,NotFound } from "../pages";
+import { UserHomePage, MangaInformation, MangaList, Library, MangaRanking,TestPage,NotFound,Profile } from "../pages";
 export const router = createBrowserRouter([
     {
         path: "/",
@@ -40,6 +40,10 @@ export const router = createBrowserRouter([
                 path: '/not-found',
                 element: <NotFound />
             },
+            {
+                path:"/profile",
+                element:<Profile/>
+            }
 
         ],
     },
