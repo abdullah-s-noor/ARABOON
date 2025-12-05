149c4d35ed2c221abd648baad31a5063f5448de4
AbdullahNoor <abdullah.s.noor04@gmail.com>
2025-12-05 14:47:59 +0200

    edit footer

--- src/components/layouts/user/layoutParts/FooterFields.jsx
+++ src/components/layouts/user/layoutParts/FooterFields.jsx
@@ -51,57 +51,58 @@ function FooterFields() {
     };
 
     const socialLinks = [
-    {
-        name: "Discord",
-        color: "#5865F2",
-        width:120,
-        accounts: [
-            { label: "Our Server", url: "https://discord.gg/YuVh3avQGA" },
-        ]
-    },
-    {
-        name: "Twitter",
-        color: "#1DA1F2",
-        accounts: [
-            // { label: "Abdullah", url: "https://x.com/REDX62409212" },
-            // { label: "Project", url: "https://x.com/project_account" }
-        ]
-    },
-    {
-        width:210,
-        name: "Facebook",
-        color: "#1877F2",
-        accounts: [
-            { label: "Abdullah Noor", url: "https://www.facebook.com/abdullah.noor.397301" },
-            { label: "Mahmoud A Darawsheh", url: "https://www.facebook.com/darxx03eh" },
-        ]
-    },
-    {
-        name: "Instagram",
-        color: "#E4405F",
-        accounts: [
-            { label: "Abdullah Noor", url: "https://www.instagram.com/abdullah.s.noor" },
-            { label: "Mahmoud A Darawsheh", url: "https://www.instagram.com/darxx03eh" },
-        ]
-    },
-    {
-        width:200,
-        name: "LinkedIn",
-        color: "#0A66C2",
-        accounts: [
-            { label: "Abdullah Noor", url: "https://www.linkedin.com/in/abdullah-s-noor" },
-            { label: "Mahmoud Darawsheh", url: "https://www.linkedin.com/in/mahmoud-darawsheh" }
-        ]
-    },
-    {
-        name: "YouTube",
-        color: "#FF0000",
-        accounts: [
-            // { label: "Channel", url: "https://www.youtube.com/@abdullahnoor8836" },
-            // { label: "Project", url: "https://youtube.com/@example" }
-        ]
-    }
-];
+        {
+            name: "Discord",
+            color: "#5865F2",
+            width: 120,
+            accounts: [
+                { label: "Our Server", url: "https://discord.gg/YuVh3avQGA" },
+            ]
+        },
+        {
+            name: "Twitter",
+            color: "#1DA1F2",
+            accounts: [
+                // { label: "Abdullah", url: "https://x.com/REDX62409212" },
+                // { label: "Project", url: "https://x.com/project_account" }
+            ]
+        },
+        {
+            width: 210,
+            name: "Facebook",
+            color: "#1877F2",
+            accounts: [
+                { label: "Abdullah Noor", url: "https://www.facebook.com/abdullah.noor.397301" },
+                { label: "Mahmoud A Darawsheh", url: "https://www.facebook.com/darxx03eh" },
+            ]
+        },
+        {
+            width: 210,
+            name: "Instagram",
+            color: "#E4405F",
+            accounts: [
+                { label: "Abdullah Noor", url: "https://www.instagram.com/abdullah.s.noor" },
+                { label: "Mahmoud A Darawsheh", url: "https://www.instagram.com/darxx03eh" },
+            ]
+        },
+        {
+            width: 200,
+            name: "LinkedIn",
+            color: "#0A66C2",
+            accounts: [
+                { label: "Abdullah Noor", url: "https://www.linkedin.com/in/abdullah-s-noor" },
+                { label: "Mahmoud Darawsheh", url: "https://www.linkedin.com/in/mahmoud-darawsheh" }
+            ]
+        },
+        {
+            name: "YouTube",
+            color: "#FF0000",
+            accounts: [
+                // { label: "Channel", url: "https://www.youtube.com/@abdullahnoor8836" },
+                // { label: "Project", url: "https://youtube.com/@example" }
+            ]
+        }
+    ];
 
 
     const socialIconMap = {
